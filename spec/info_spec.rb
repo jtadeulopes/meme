d1b26fa1d5464b3dd87da9249d10d303318fc01e
Jesus Lopes <jlopes@zigotto.com.br>
2010-04-21 00:39:40 -0300

    Refactor specs.

--- spec/info_spec.rb
+++ spec/info_spec.rb
@@ -2,46 +2,48 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Meme::Info" do
 
-  describe "#user not found" do
-    it "should return nil" do
-      query = "SELECT * FROM meme.info WHERE name='memeusernotfound'"
-      fake_web(query, 'meme_info_not_found.json')
-      Meme::Info.find('memeusernotfound').should be_nil
-    end
-  end
-
   before :each do
     query = "SELECT * FROM meme.info WHERE name='jtadeulopes'"
     fake_web(query, 'meme_info.json')
     @profile = Meme::Info.find('jtadeulopes')
   end
 
-  it "should return name" do
-    @profile.name.should == "jtadeulopes"
-  end
+  describe "#find" do
 
-  it "should return guid" do
-    @profile.guid.should == "EMREXCV4R5OTM3CZW3HBD5QAGY"
-  end
+    it "if user not found, should return nil" do
+      query = "SELECT * FROM meme.info WHERE name='memeusernotfound'"
+      fake_web(query, 'meme_info_not_found.json')
+      Meme::Info.find('memeusernotfound').should be_nil
+    end
 
-  it "should return page title" do
-    @profile.title.should == "Jésus Lopes"
-  end
+    it "should return name" do
+      @profile.name.should == "jtadeulopes"
+    end
 
-  it "should return description" do
-    @profile.description.should == "software developer"
-  end
+    it "should return guid" do
+      @profile.guid.should == "EMREXCV4R5OTM3CZW3HBD5QAGY"
+    end
 
-  it "should return url" do
-    @profile.url.should == "http://meme.yahoo.com/jtadeulopes/"
-  end
+    it "should return page title" do
+      @profile.title.should == "Jésus Lopes"
+    end
 
-  it "should return avatar" do
-    @profile.avatar_url.should == "http://d.yimg.com/gg/jtadeulopes/avatars/44251e70378d4d225f8cda09a6c3aec87301833a.jpeg"
-  end
+    it "should return description" do
+      @profile.description.should == "software developer"
+    end
 
-  it "should return language" do
-    @profile.language.should == "pt"
+    it "should return url" do
+      @profile.url.should == "http://meme.yahoo.com/jtadeulopes/"
+    end
+
+    it "should return avatar" do
+      @profile.avatar_url.should == "http://d.yimg.com/gg/jtadeulopes/avatars/44251e70378d4d225f8cda09a6c3aec87301833a.jpeg"
+    end
+
+    it "should return language" do
+      @profile.language.should == "pt"
+    end
+
   end
 
   describe "#followers" do
@@ -67,21 +69,25 @@ describe "Meme::Info" do
   end
 
   describe "#following" do
+
     it "should return following" do
       query = "SELECT * FROM meme.following(10) WHERE owner_guid='EMREXCV4R5OTM3CZW3HBD5QAGY'"
       fake_web(query, 'meme_following.json')
       @profile.following.count.should == 10
     end
+
     it "should return seven following" do
       query = "SELECT * FROM meme.following(7) WHERE owner_guid='EMREXCV4R5OTM3CZW3HBD5QAGY'"
       fake_web(query, 'meme_following_count.json')
       @profile.following(7).count.should == 7
     end
+
     it "should return all following" do
       query = "SELECT * FROM meme.following(0) WHERE owner_guid='EMREXCV4R5OTM3CZW3HBD5QAGY'"
       fake_web(query, 'meme_following_all.json')
       @profile.following(:all).count.should == 39
     end
+
   end
 
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,73 +1,75 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Meme::Search" do
+describe "Meme::Post" do
 
-  before :each do
-    query = "SELECT * FROM meme.search WHERE query='meme rocks'"
-    fake_web(query, 'meme_search.json')
-    @results = Meme::Post.find('meme rocks')
-  end
+  describe "#find" do
 
-  it "should return pubid" do
-    @results.first.pubid.should == "yC8nqOd"
-  end
+    before :each do
+      query = "SELECT * FROM meme.search WHERE query='meme rocks'"
+      fake_web(query, 'meme_search.json')
+      @results = Meme::Post.find('meme rocks')
+    end
 
-  it "should return guid" do
-    @results.first.guid.should == "7Z7XFAPD5FLXFKJVT4NG6XTTA4"
-  end
+    it "should return pubid" do
+      @results.first.pubid.should == "yC8nqOd"
+    end
 
-  it "should return url" do
-    @results.first.url.should == "http://meme.yahoo.com/celizelove/p/yC8nqOd/"
-  end
+    it "should return guid" do
+      @results.first.guid.should == "7Z7XFAPD5FLXFKJVT4NG6XTTA4"
+    end
 
-  it "should return timestamp" do
-    @results.first.timestamp.should == "1268410909000"
-  end
+    it "should return url" do
+      @results.first.url.should == "http://meme.yahoo.com/celizelove/p/yC8nqOd/"
+    end
 
-  it "should return category" do
-    @results.first.category.should == "text"
-  end
+    it "should return timestamp" do
+      @results.first.timestamp.should == "1268410909000"
+    end
 
-  it "should return type" do
-    @results.first.type.should == "text"
-  end
+    it "should return category" do
+      @results.first.category.should == "text"
+    end
 
-  it "should return content" do
-    @results[4].content.should == "http://d.yimg.com/gg/u/2441eceb4b334c6fc29c9bd306fe1957693d605e.jpeg"
-  end
+    it "should return type" do
+      @results.first.type.should == "text"
+    end
 
-  it "should return repost_count" do
-    @results.first.repost_count.should == "0"
-  end
+    it "should return content" do
+      @results[4].content.should == "http://d.yimg.com/gg/u/2441eceb4b334c6fc29c9bd306fe1957693d605e.jpeg"
+    end
 
-  it "should return caption" do
-    @results[1].caption.should == "This is good. But a lot of things happening means a high chance that I, the man who lives and breathes Panic and has a giant status board in my head, might not properly explain everything to everyone. Steve and I realized it was high time we made this Cabel Status Board public… using technology!"
-  end
+    it "should return repost_count" do
+      @results.first.repost_count.should == "0"
+    end
 
-  describe "#search by" do
+    it "should return caption" do
+      @results[1].caption.should == "This is good. But a lot of things happening means a high chance that I, the man who lives and breathes Panic and has a giant status board in my head, might not properly explain everything to everyone. Steve and I realized it was high time we made this Cabel Status Board public… using technology!"
+    end
 
-    describe "#posts not found" do
-      it "should return nil" do
-        query = "SELECT * FROM meme.search WHERE query='brhackday' and type='audio'"
-        fake_web(query, 'meme_search_type_audio.json')
-        Meme::Post.find('brhackday', :type => :audio).should be_nil
+    it "if posts not found, should return nil" do
+      query = "SELECT * FROM meme.search WHERE query='brhackday' and type='audio'"
+      fake_web(query, 'meme_search_type_audio.json')
+      Meme::Post.find('brhackday', :type => :audio).should be_nil
+    end
+
+    context "using the option :type" do
+
+      it "type photo" do
+        query = "SELECT * FROM meme.search WHERE query='meme rocks' and type='photo'"
+        fake_web(query, 'meme_search_type_photo.json')
+        @results = Meme::Post.find('meme rocks', :type => :photo)
+        @results.count.should == 3
+        @results.first.type.should == "photo"
       end
-    end
 
-    it "type photo" do
-      query = "SELECT * FROM meme.search WHERE query='meme rocks' and type='photo'"
-      fake_web(query, 'meme_search_type_photo.json')
-      @results = Meme::Post.find('meme rocks', :type => :photo)
-      @results.count.should == 3
-      @results.first.type.should == "photo"
-    end
+      it "type video" do
+        query = "SELECT * FROM meme.search WHERE query='keyboard cat' and type='video'"
+        fake_web(query, 'meme_search_type_video.json')
+        @results = Meme::Post.find('keyboard cat', :type => :video)
+        @results.count.should == 2
+        @results.first.type.should == "video"
+      end
 
-    it "type video" do
-      query = "SELECT * FROM meme.search WHERE query='keyboard cat' and type='video'"
-      fake_web(query, 'meme_search_type_video.json')
-      @results = Meme::Post.find('keyboard cat', :type => :video)
-      @results.count.should == 2
-      @results.first.type.should == "video"
     end
 
   end
